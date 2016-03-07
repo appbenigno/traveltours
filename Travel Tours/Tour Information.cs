218aedba6b7a379c60456876f7ba918b8cb20a64
DeltaTwoThree <m_caracta@yahoo.com>
2016-03-07 22:29:49 +0800

    Yan inadd ko na for realz yung Tour Information Window

---
+++ Travel Tours/Tour Information.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Travel_Tours
+{
+    public partial class Tour_Information : Form
+    {
+        public Tour_Information()
+        {
+            InitializeComponent();
+        }
+
+        private void Tour_Information_Load(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Hue Hue Hue
+
+        }
+    }
+}
---
+++ Travel Tours/Tour Information.designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Travel_Tours
+{
+    partial class Tour_Information
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.texBox_TourCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker_START = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker_END = new System.Windows.Forms.DateTimePicker();
+            this.textBox_PAX = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.PreviewInfo_Button = new System.Windows.Forms.Button();
+            this.Itineraries_Button = new System.Windows.Forms.Button();
+            this.Cancel_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(468, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tour Code:";
+            // 
+            // texBox_TourCode
+            // 
+            this.texBox_TourCode.Location = new System.Drawing.Point(534, 38);
+            this.texBox_TourCode.Name = "texBox_TourCode";
+            this.texBox_TourCode.ReadOnly = true;
+            this.texBox_TourCode.Size = new System.Drawing.Size(100, 20);
+            this.texBox_TourCode.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name of Company/Personnel/School etc.  :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(232, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Date of Tour:";
+            // 
+            // dateTimePicker_START
+            // 
+            this.dateTimePicker_START.Location = new System.Drawing.Point(12, 94);
+            this.dateTimePicker_START.Name = "dateTimePicker_START";
+            this.dateTimePicker_START.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker_START.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(259, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "End date of Tour:";
+            // 
+            // dateTimePicker_END
+            // 
+            this.dateTimePicker_END.Location = new System.Drawing.Point(262, 94);
+            this.dateTimePicker_END.Name = "dateTimePicker_END";
+            this.dateTimePicker_END.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker_END.TabIndex = 8;
+            // 
+            // textBox_PAX
+            // 
+            this.textBox_PAX.Location = new System.Drawing.Point(123, 140);
+            this.textBox_PAX.Name = "textBox_PAX";
+            this.textBox_PAX.Size = new System.Drawing.Size(119, 20);
+            this.textBox_PAX.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Total number of Pax:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 243);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Number of Bus/es:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(114, 240);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(128, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 193);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(175, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Number of Tour Guides/Facilitators:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(190, 190);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(52, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(262, 140);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(372, 213);
+            this.richTextBox1.TabIndex = 15;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // PreviewInfo_Button
+            // 
+            this.PreviewInfo_Button.Location = new System.Drawing.Point(496, 91);
+            this.PreviewInfo_Button.Name = "PreviewInfo_Button";
+            this.PreviewInfo_Button.Size = new System.Drawing.Size(138, 23);
+            this.PreviewInfo_Button.TabIndex = 16;
+            this.PreviewInfo_Button.Text = "Information Preview";
+            this.PreviewInfo_Button.UseVisualStyleBackColor = true;
+            // 
+            // Itineraries_Button
+            // 
+            this.Itineraries_Button.Location = new System.Drawing.Point(12, 283);
+            this.Itineraries_Button.Name = "Itineraries_Button";
+            this.Itineraries_Button.Size = new System.Drawing.Size(230, 30);
+            this.Itineraries_Button.TabIndex = 17;
+            this.Itineraries_Button.Text = "Itineraries";
+            this.Itineraries_Button.UseVisualStyleBackColor = true;
+            // 
+            // Cancel_Button
+            // 
+            this.Cancel_Button.Location = new System.Drawing.Point(12, 323);
+            this.Cancel_Button.Name = "Cancel_Button";
+            this.Cancel_Button.Size = new System.Drawing.Size(230, 30);
+            this.Cancel_Button.TabIndex = 18;
+            this.Cancel_Button.Text = "Cancel";
+            this.Cancel_Button.UseVisualStyleBackColor = true;
+            // 
+            // Tour_Information
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 365);
+            this.Controls.Add(this.Cancel_Button);
+            this.Controls.Add(this.Itineraries_Button);
+            this.Controls.Add(this.PreviewInfo_Button);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_PAX);
+            this.Controls.Add(this.dateTimePicker_END);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker_START);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.texBox_TourCode);
+            this.Controls.Add(this.label1);
+            this.Name = "Tour_Information";
+            this.Text = "Tour Information Window";
+            this.Load += new System.EventHandler(this.Tour_Information_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox texBox_TourCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_START;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_END;
+        private System.Windows.Forms.TextBox textBox_PAX;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button PreviewInfo_Button;
+        private System.Windows.Forms.Button Itineraries_Button;
+        private System.Windows.Forms.Button Cancel_Button;
+    }
+}
