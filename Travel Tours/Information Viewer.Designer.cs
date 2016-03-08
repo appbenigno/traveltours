6db46a34f43287f64a28e5974b7cfdbb3300aa1a
DeltaTwoThree <m_caracta@yahoo.com>
2016-03-08 20:47:04 +0800

    Tour Information Viewer added

---
+++ Travel Tours/Information Viewer.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Travel_Tours
+{
+    partial class Information_Viewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Elmanjovin_Database_Viewer = new System.Windows.Forms.DataGridView();
+            this.comboBox_Information_Viewer = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_Edit_Info = new System.Windows.Forms.Button();
+            this.comboBox_Ordering = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Elmanjovin_Database_Viewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Elmanjovin_Database_Viewer
+            // 
+            this.Elmanjovin_Database_Viewer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Elmanjovin_Database_Viewer.Location = new System.Drawing.Point(12, 156);
+            this.Elmanjovin_Database_Viewer.Name = "Elmanjovin_Database_Viewer";
+            this.Elmanjovin_Database_Viewer.Size = new System.Drawing.Size(963, 341);
+            this.Elmanjovin_Database_Viewer.TabIndex = 0;
+            // 
+            // comboBox_Information_Viewer
+            // 
+            this.comboBox_Information_Viewer.FormattingEnabled = true;
+            this.comboBox_Information_Viewer.Items.AddRange(new object[] {
+            "Tour Information",
+            "Itinerary Information",
+            "Bus Information",
+            "Tour Guides"});
+            this.comboBox_Information_Viewer.Location = new System.Drawing.Point(12, 37);
+            this.comboBox_Information_Viewer.Name = "comboBox_Information_Viewer";
+            this.comboBox_Information_Viewer.Size = new System.Drawing.Size(159, 21);
+            this.comboBox_Information_Viewer.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Information to be viewed:";
+            // 
+            // button_Edit_Info
+            // 
+            this.button_Edit_Info.Location = new System.Drawing.Point(12, 94);
+            this.button_Edit_Info.Name = "button_Edit_Info";
+            this.button_Edit_Info.Size = new System.Drawing.Size(159, 23);
+            this.button_Edit_Info.TabIndex = 3;
+            this.button_Edit_Info.Text = "Edit Information";
+            this.button_Edit_Info.UseVisualStyleBackColor = true;
+            // 
+            // comboBox_Ordering
+            // 
+            this.comboBox_Ordering.FormattingEnabled = true;
+            this.comboBox_Ordering.Location = new System.Drawing.Point(177, 37);
+            this.comboBox_Ordering.Name = "comboBox_Ordering";
+            this.comboBox_Ordering.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_Ordering.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Order by:";
+            // 
+            // Information_Viewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 509);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox_Ordering);
+            this.Controls.Add(this.button_Edit_Info);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox_Information_Viewer);
+            this.Controls.Add(this.Elmanjovin_Database_Viewer);
+            this.Name = "Information_Viewer";
+            this.Text = "Tour Information Viewer";
+            this.Load += new System.EventHandler(this.Information_Viewer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Elmanjovin_Database_Viewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Elmanjovin_Database_Viewer;
+        private System.Windows.Forms.ComboBox comboBox_Information_Viewer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_Edit_Info;
+        private System.Windows.Forms.ComboBox comboBox_Ordering;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Travel Tours/Information Viewer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Travel_Tours
+{
+    public partial class Information_Viewer : Form
+    {
+        public Information_Viewer()
+        {
+            InitializeComponent();
+        }
+
+        private void Information_Viewer_Load(object sender, EventArgs e)
+        {
+            // Tour Information Viewer window
+            // Displays information and tour details
+            // FOR TOUR FACILITATORS: ANYTHING IS EDITABLE AND DELETABLE
+        }
+    }
+}
