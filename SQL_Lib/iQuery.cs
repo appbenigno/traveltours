d7a98dfad867a8f8c9dfad098fab7b174a71ecea
DeltaTwoThree <m_caracta@yahoo.com>
2016-03-10 20:26:59 +0800

    Add, Edit, Delete methods in iQuery.cs have been updated

--- SQL_Lib/iQuery.cs
+++ SQL_Lib/iQuery.cs
@@ -11,9 +11,9 @@ namespace SQL_Lib
 {
     interface iQuery
     {
-        void Add();
-        void Edit();
-        void Delete();
+        void Add(int Tour_Code, string Company_Information, string Tour_Start_Date, string Tour_End_Date, int Number_Of_Passengers, int Number_Of_Tour_Guides, int Number_Of_Buses);
+        void Edit(string Value_To_Be_Edited);
+        void Delete(int Key_Identifier);
         bool isExist(string tableName, string primaryKeyField, string primaryKeyValue, string targetFieldName, string stringMatch);
         string getData(string tableName, string primaryKeyField, string primaryKeyValue, string targetFieldName);
         void CommandExec(string selectCommand, DataGridView targetDataGridView);
