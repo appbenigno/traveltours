06991af9df57fe374d3021117d3b10c87f185d0c
appbenigno <appbenigno@live.com.ph>
2016-03-13 18:03:15 +0800

    working add schedule

--- Travel Tours/Information Viewer.cs
+++ Travel Tours/Information Viewer.cs
@@ -54,9 +54,11 @@ namespace Travel_Tours
 
         private void button_Edit_Info_Click(object sender, EventArgs e)
         {
-            Edit_Information Edit_Info_Window = new Edit_Information();
+        }
 
-            Edit_Info_Window.Show();
+        private void button_DELETE_INFORMATION_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
